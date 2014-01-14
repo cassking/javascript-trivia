26cdeb97edca61ef526631324ed52cfa6b6490f0
ccressman <ccressman@weblinc.com>
2014-01-14 01:09:41 -0500

    Add first two questions

---
+++ trivia.js
@@ -0,0 +1,37 @@
+var trivia = [
+
+
+
+    // 0.
+
+    function () {
+        var one = 1;
+        var One = 1;
+
+        return one === One;
+    },
+
+    // A. true
+    // B. false
+    // C. (error)
+    // D. (none of the above)
+
+
+
+    // 1.
+
+    function () {
+        var one = {};
+        var one = {};
+
+        return one === one;
+    }
+
+    // A. true
+    // B. false
+    // C. (error)
+    // D. (none of the above)
+
+
+
+];
